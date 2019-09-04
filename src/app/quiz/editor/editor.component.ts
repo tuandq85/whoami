6eb61c9ca59c262ed144785bc4dbebbc2702b0d3
Dang Quang Tuan <quangtuanhm@gmail.com>
2019-09-04 23:58:29 +0700

    Upload image.

--- src/app/quiz/editor/editor.component.ts
+++ src/app/quiz/editor/editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RichTextEditor } from '@syncfusion/ej2-richtexteditor';
 
 @Component({
   selector: 'app-editor',
@@ -10,67 +11,23 @@ export class EditorComponent implements OnInit {
 
   questionType = null;
   editorValue = null;
+  imageSettings: Object = null;
 
-  uploadSettings = {
-    type: 'post',
-    url: 'http://localhost:8080/upload/saveFile'
-  };
-  removeSettings = {
-    type: 'post',
-    url: 'http://localhost:8080/upload/removeFile'
-  };
+  constructor(private http: HttpClient) {
+    let imgPath = "/demoURL/folder";
 
-  imgPath: string = null;
-  public imageSettings: Object = {
-    // saveUrl: this.uploadSettings,
-    saveUrl: 'http://localhost:8080/upload/saveFile',
-    // removeUrl: this.removeSettings,
-    path: this.imgPath,
-    display: 'inline'
-  };
-
-
-
-  constructor(private http: HttpClient) { }
+    this.imageSettings = {
+      // saveUrl: this.uploadSettings,
+      saveUrl: 'http://localhost:8080/upload/saveFile',
+      // removeUrl: this.removeSettings,
+      path: imgPath,
+      display: 'inline'
+    };
+  }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    console.log('Question type: ', this.questionType);
-    console.log('Editor value:', this.editorValue);
-
-    // get blob.
-    var indexOfString = this.editorValue.substring(this.editorValue.indexOf("blob:http"));
-    indexOfString = indexOfString.substring(0, indexOfString.indexOf("\""));
-    console.log(indexOfString.substring("blod:".length));
-
-    var blob = new Blob([indexOfString]);
-    let reader = new FileReader;
-
- reader.readAsDataURL(blob); // read file as data url
- reader.onload = () => { // when file has loaded
-    console.log(reader.result)
-    var img:any = new Image();
-    img.src = reader.result;
-
-    img.onload = () => {
-      console.log("image width: ", img.width); //to get image width
-      console.log("image height: ", img.height);  //to get image height
-      console.log("Upload url: ", reader.result); //to get blob image
-      console.log(reader.result)          
-    };          
-  } 
-
   }
-
-  // public onToolbarClick(e: any): void {
-  //   if (e.item != null && e.item.id == "imageRTE_toolbar_Image") { // Checked if image toolbar is clicked 
-  //     let element: any = document.getElementById('imageRTE_upload') // Image uploader element 
-  //     element.ej2_instances[0].uploading = function upload(args) { // Added updating event on image uploader 
-  //       console.log("Go in here");
-  //       console.log("detail:", args);
-  //     }
-  //   }
-  // }
 }
